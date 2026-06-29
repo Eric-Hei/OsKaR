5e387d50755f90bbd939e7cd022bb703be5feb65
Eric H <erich34@gmail.com>
2026-06-29 18:18:53 +0200

    chore: update version to 2.1.1 in package.json and APP_CONFIG

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AmbitionCategory, Priority, Quarter } from '@/types';
 // Configuration de l'application
 export const APP_CONFIG = {
   name: 'OsKaR',
-  version: '2.1.0',
+  version: '2.1.1',
   description: 'Outil de gestion d\'objectifs pour entrepreneurs avec IA coach',
   author: 'OsKaR Team',
   maxAmbitionsPerYear: 5,
